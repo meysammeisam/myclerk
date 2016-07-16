918feb7f675276f25e5d2584a5e5222840c92d41
MeysaM <meysammeisam@gmail.com>
2016-07-17 02:05:18 +0430

    add user#signup tests

--- test/integration/session_registration_test.rb
+++ test/integration/session_registration_test.rb
@@ -5,13 +5,32 @@ class SessionRegistrationTest < ActionDispatch::IntegrationTest
     @user1 = users(:user1)
   end
 
-  test "test" do
-    assert true
+  test "must not signUp with invalid information" do
+    get "/sign_up"
+    assert_no_difference 'User.count' do
+      post "/sign_up", user: { username:  "",                # username must not be blank
+                               email: "user@invalid",        # invalid email
+                               password:              "foo", # password is too short
+                               password_confirmation: "bar"  # password not match
+                              }
+    end
+    assert response.body.include? "3 errors prohibited this user from being saved:"
+    assert response.body.include? "Email is invalid"
+    assert response.body.include? "Password is too short (minimum is 8 characters)"
+    assert_template 'clerk_first/user/registrations/new'
+  end
+
+  test "must signUp with valid information" do
+    get "/sign_up"
+    assert_difference 'User.count', 1 do
+      post "/sign_up", user: { username: "XXX", email: "user@gmail.com", password: "12345678", password_confirmation: "12345678" }
+    end
+    assert_response 302 #redirected
   end
 
   test "registration page form test" do
-    get "/"
-    assert_response :success
+    #get "/"
+    #assert_response :success
   end
 
 end
